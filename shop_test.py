a56f8a78088bc20fc7dc70d4f4d918dd4c352e2e
ironfrancis <82508241@qq.com>
2022-05-15 11:33:16 +0800

    Revert "Member类"
    This reverts commit 9a8dab64

--- shop_test.py
+++ shop_test.py
@@ -4,5 +4,8 @@ import pandas as pd
 from BaseClass.Shop import gtpShop
 
 # print(gtpShop.pos.LoadProductSaleDetailsByPage())
-
-print(gtpShop.pos.LoadCustomersByPage())
+mblist = gtpShop.pos.LoadProductSaleDetailsByPage()['会员卡号'].unique()
+for mb in mblist:
+    print(mb)
+    # print(gtpShop.pos.LoadProductSaleDetailsByPage(mb))
+    print("-" * 50)
